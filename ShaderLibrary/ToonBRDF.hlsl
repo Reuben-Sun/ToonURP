b6dd69b58757a73ed417850a0461a22b32099a12
Reuben SF <sunzheng@stepfun.com>
2024-07-17 16:22:04 +0800

    lighting data

--- ShaderLibrary/ToonBRDF.hlsl
+++ ShaderLibrary/ToonBRDF.hlsl
@@ -1,13 +1,26 @@
 ﻿#ifndef TOON_BRDF_INCLUDED
 #define TOON_BRDF_INCLUDED
 
-// void FernInitializeBRDFData(ToonSurfaceData surfaceData, out BRDFData outBRDFData, out BRDFData outClearBRDFData)
-// {
-//     InitializeBRDFData(surfaceData.albedo, surfaceData.metallic, surfaceData.specular, surfaceData.smoothness, surfaceData.alpha, outBRDFData);
-//     outClearBRDFData = outBRDFData;
-//     #if _CLEARCOAT
-//     outClearBRDFData = CreateNPRClearCoatBRDFData(surfaceData, outBRDFData);
-//     #endif
-// }
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/BRDF.hlsl"
+
+void InitializeToonBRDFData(ToonSurfaceData surfaceData, out BRDFData outBRDFData)
+{
+    half oneMinusReflectivity = OneMinusReflectivityMetallic(surfaceData.metallic);
+    half reflectivity = half(1.0) - oneMinusReflectivity;
+    half3 brdfDiffuse = surfaceData.albedo * oneMinusReflectivity;
+    half3 brdfSpecular = lerp(kDieletricSpec.rgb, surfaceData.albedo, surfaceData.metallic);
+
+    outBRDFData = (BRDFData)0;
+    outBRDFData.albedo = surfaceData.albedo;
+    outBRDFData.diffuse = brdfDiffuse;
+    outBRDFData.specular = brdfSpecular;
+    outBRDFData.reflectivity = reflectivity;
+    outBRDFData.perceptualRoughness = surfaceData.roughness;
+    outBRDFData.roughness           = max(PerceptualRoughnessToRoughness(outBRDFData.perceptualRoughness), HALF_MIN_SQRT);
+    outBRDFData.roughness2          = max(outBRDFData.roughness * outBRDFData.roughness, HALF_MIN);
+    outBRDFData.grazingTerm         = saturate((1.0f-surfaceData.roughness) + reflectivity);
+    outBRDFData.normalizationTerm   = outBRDFData.roughness * half(4.0) + half(2.0);
+    outBRDFData.roughness2MinusOne  = outBRDFData.roughness2 - half(1.0);
+}
 
 #endif
--- ShaderLibrary/ToonLighting.hlsl
+++ ShaderLibrary/ToonLighting.hlsl
@@ -5,6 +5,38 @@
 #include "Packages/com.reubensun.toonurp/ShaderLibrary/ToonBRDF.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
+struct ToonLightingData
+{
+    float3 lightColor;
+    float3 halfDir;
+    float3 lightDir;
+    float NoL;
+    float NoLClamp;
+    float HalfLambert;
+    float NoVClamp;
+    float NoHClamp;
+    float LoHClamp;
+    float VoHClamp;
+    float shadowAttenuation;
+};
+
+ToonLightingData InitializeLightingData(Light mainLight, float3 normalWS, float3 viewDir)
+{
+    ToonLightingData lightData = (ToonLightingData)0;
+    lightData.lightColor = mainLight.color;
+    lightData.lightDir = mainLight.direction.xyz;
+    lightData.NoL = dot(normalWS, lightData.lightDir);
+    lightData.NoLClamp = saturate(lightData.NoL);
+    lightData.HalfLambert = 0.5 * (lightData.NoL + 1);
+    lightData.NoVClamp = saturate(dot(normalWS, viewDir));
+    half3 halfDir = SafeNormalize(lightData.lightDir + viewDir);
+    lightData.halfDir = halfDir;
+    lightData.NoHClamp = saturate(dot(normalWS, halfDir));
+    lightData.LoHClamp = saturate(dot(lightData.lightDir, halfDir));
+    lightData.shadowAttenuation = mainLight.shadowAttenuation * mainLight.distanceAttenuation;
+    return lightData;
+}
+
 float4 UniversalFragmentPBR(InputData inputData, ToonSurfaceData toonSurfaceData)
 {
     SurfaceData surfaceData = ConvertToonSurfaceDataToURPSurfaceData(toonSurfaceData);
@@ -18,6 +50,7 @@ float4 ToonFragment(InputData inputData, ToonSurfaceData toonSurfaceData)
     uint meshRenderingLayers = GetMeshRenderingLayer();
     Light mainLight = GetMainLight(inputData.shadowCoord, inputData.positionWS, shadowMask);
     MixRealtimeAndBakedGI(mainLight, inputData.normalWS, inputData.bakedGI);
+
     #if defined(_SCREEN_SPACE_OCCLUSION)
     AmbientOcclusionFactor aoFactor = GetScreenSpaceAmbientOcclusion(inputData.normalizedScreenSpaceUV);
     mainLight.color *= aoFactor.directAmbientOcclusion;
@@ -27,12 +60,13 @@ float4 ToonFragment(InputData inputData, ToonSurfaceData toonSurfaceData)
     aoFactor.indirectAmbientOcclusion = 1;
     aoFactor.directAmbientOcclusion = 1;
     #endif
-    BRDFData brdfData, clearCoatbrdfData;
-    // InitializeBRDFData(surfaceData, brdfData, clearCoatbrdfData);
+
+    BRDFData brdfData;
+    InitializeToonBRDFData(toonSurfaceData, brdfData);
 
     // lighting
-    // LightingData lightingData = InitializeLightingData(mainLight, input, inputData.normalWS, inputData.viewDirectionWS, addInputData);
-
+    ToonLightingData lightingData = InitializeLightingData(mainLight, inputData.normalWS, inputData.viewDirectionWS);
+    
     half4 color = 1;
     // color.rgb = FernMainLightDirectLighting(brdfData, clearCoatbrdfData, input, inputData, surfaceData, lightingData);
     // color.rgb += FernAdditionLightDirectLighting(brdfData, clearCoatbrdfData, input, inputData, surfaceData, addInputData, shadowMask, meshRenderingLayers, aoFactor);
