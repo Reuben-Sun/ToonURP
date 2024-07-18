8e94b8135334f4cbe8aa55b4afebd96af160fb33
Reuben SF <sunzheng@stepfun.com>
2024-07-18 17:25:50 +0800

    edge outline init

--- Runtime/EdgeOutline.cs
+++ Runtime/EdgeOutline.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class EdgeOutline : MonoBehaviour
+namespace ToonURP
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable, VolumeComponentMenu("ToonURP/Edge Outline")]
+    public class EdgeOutline : VolumeComponent, IPostProcessComponent
     {
-        
-    }
+        [Tooltip("Controls the Effect Intensity")]
+        public ClampedFloatParameter intensity = new ClampedFloatParameter(0, 0, 1);
+        public bool IsActive()
+        {
+            return intensity.value > 0;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public bool IsTileCompatible()
+        {
+            return false;
+        }
     }
 }
