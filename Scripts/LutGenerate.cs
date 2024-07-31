fa4fca2cb9edcd7bfed2935ae804fab88e920515
Reuben SF <sunzheng@stepfun.com>
2024-07-31 11:46:49 +0800

    蒙特卡洛

--- Scripts/LutGenerate.cs
+++ Scripts/LutGenerate.cs
@@ -21,13 +21,39 @@ namespace ToonURP
             return h * sum;
         }
     }
+    public class MonteCarlo
+    {
+        public static double Integration(Func<double, double> function, double begin, double end, int sampleCount)
+        {
+            System.Random random = new System.Random();
+            double sum = 0.0;
+
+            for (int i = 0; i < sampleCount; i++)
+            {
+                double x = begin + (end - begin) * random.NextDouble();
+                sum += function(x);
+            }
+
+            double average = sum / sampleCount;
+            return (end - begin) * average;
+        }
+    }
+    
     public class LutGenerate : MonoBehaviour
     {
         void Start()
         {
             Func<double, double> cosFunction = x => -Math.Sqrt(1 - x * x);
             double ans = IntegralCalculator.Calculate(cosFunction, 0, 1, 100);
-            Debug.Log(ans);
+            Debug.Log($"calc ans: {ans}");
+            double ans2 = MonteCarlo.Integration(cosFunction, 0, 1, 10);
+            Debug.Log($"monte carlo count 10 ans: {ans2}");
+            ans2 = MonteCarlo.Integration(cosFunction, 0, 1, 100);
+            Debug.Log($"monte carlo count 100 ans: {ans2}");
+            ans2 = MonteCarlo.Integration(cosFunction, 0, 1, 500);
+            Debug.Log($"monte carlo count 500 ans: {ans2}");
+            ans2 = MonteCarlo.Integration(cosFunction, 0, 1, 1000);
+            Debug.Log($"monte carlo count 1000 ans: {ans2}");
         }
     }
 }
