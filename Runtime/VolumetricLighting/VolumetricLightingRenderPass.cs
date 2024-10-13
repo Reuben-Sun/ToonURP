c2e25a1e955863991fa3523215ee9a3703c668c7
Reuben-PC <2824016820@qq.com>
2024-10-13 22:34:08 +0800

    TODO

--- Runtime/VolumetricLighting/VolumetricLightingRenderPass.cs
+++ Runtime/VolumetricLighting/VolumetricLightingRenderPass.cs
@@ -7,7 +7,9 @@ namespace ToonURP
     {
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            
+            // 1. 屏幕 UV -> 世界坐标，利用深度图做 ray matching，如果某点世界坐标不再阴影中，则累计光照
+            // 2. 对累计光照进行高斯模糊
+            // 3. 将高斯模糊后的光照叠加到场景中
         }
     }
 }
